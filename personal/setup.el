11f2b34e540a4d8f066579de9553fa8f4a31de14
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-09-06 12:07:24 +0200

    guru-mode warn only

--- personal/setup.el
+++ personal/setup.el
@@ -1,2 +1,9 @@
+;;; package -- setup
+;;; commentary :
+
+;;; set the frame to be transparent
 (set-frame-parameter (selected-frame) 'alpha '(90 90))
 (add-to-list 'default-frame-alist '(alpha 90 90))
+
+;;; warn only about
+(setq guru-warn-only t)
