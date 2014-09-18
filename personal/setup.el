213d91aafb3162ffe274b9decefbe7517c63ae34
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2014-09-18 23:43:58 +0200

    added config for yasnippet

--- personal/setup.el
+++ personal/setup.el
@@ -7,3 +7,14 @@
 
 ;;; warn only about
 (setq guru-warn-only t)
+
+(prelude-require-packages '(yasnippet))
+(add-to-list 'load-path (concat prelude-personal-dir "/snippets"))
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(prelude-require-packages '(multiple-cursors))
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
